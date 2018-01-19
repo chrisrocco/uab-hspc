3e43b5d7fba8e595dd50105c4ac7dd986e0ca7e0
= <=>
2018-01-18 23:44:58 -0600

    added error reporter

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,6 @@
 use Illuminate\Http\Request;
 use App\Registrations;
 
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/register', function(Request $request){
     if(count(Registrations::where('email', '=', $request->email)->get()) > 0) abort(409);
 
